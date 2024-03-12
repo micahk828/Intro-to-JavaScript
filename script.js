032c11c6277f12015ded81fad14814ef47a7755e
Micah Kennedy <144722193+micahk828@users.noreply.github.com>
2024-03-12 00:26:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function myfunction() {
+    document.getElementById("greet").style.color="red";
+
+}
+
+function night() {
+  document.body.style.backgroundColor="darkblue";
+}
+
+function paragraph() {
+  document.getElementById("change").style.color="blue";
+}
